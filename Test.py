acc6d9617dcbe891924ed6936e84dfade862cd13
Abhishek Ramane <35802001+abhishek-ramane@users.noreply.github.com>
2021-06-03 19:44:26 +0530

    modified Test.py for equity options

--- Test.py
+++ Test.py
@@ -1,9 +1,67 @@
-from jugaad_data.nse import NSELive
+from live import NSELive
 import streamlit as st
+from pynse import *
+import streamlit as st
+import logging
+import pandas as pd
+import datetime
+import json
 
 n = NSELive()
 option_chain = n.index_option_chain("NIFTY")
 
-for option in option_chain['filtered']['data']:
-    st.write("CE_LastPrice:{}\tstrikePrice:{}\tPE_LastPrice:{}".format(option['CE']['lastPrice'], option['strikePrice'],
-                                                                       option['PE']['lastPrice']))
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s;%(levelname)s;%(message)s")
+
+symbols = ["AARTIIND", "ACC", "ADANIENT", "ADANIPORTS", "ALKEM", "AMARAJABAT", "AMBUJACEM", "APLLTD", "APOLLOHOSP",
+           "APOLLOTYRE", "ASHOKLEY", "ASIANPAINT", "AUBANK", "AUROPHARMA", "AXISBANK", "BAJAJ-AUTO", "BAJAJFINSV",
+           "BAJFINANCE", "BALKRISIND", "BANDHANBNK", "BANKBARODA", "BATAINDIA", "BEL", "BERGEPAINT", "BHARATFORG",
+           "BHARTIARTL", "BHEL", "BIOCON", "BOSCHLTD", "BPCL", "BRITANNIA", "CADILAHC", "CANBK", "CHOLAFIN", "CIPLA",
+           "COALINDIA", "COFORGE", "COLPAL", "CONCOR", "CUB", "CUMMINSIND", "DABUR", "DEEPAKNTR", "DIVISLAB", "DLF",
+           "DRREDDY", "EICHERMOT", "ESCORTS", "EXIDEIND", "FEDERALBNK", "GAIL", "GLENMARK", "GMRINFRA", "GODREJCP",
+           "GODREJPROP", "GRANULES", "GRASIM", "GUJGASLTD", "HAVELLS", "HCLTECH", "HDFC", "HDFCAMC", "HDFCBANK",
+           "HDFCLIFE", "HEROMOTOCO", "HINDALCO", "HINDPETRO", "HINDUNILVR", "IBULHSGFIN", "ICICIBANK", "ICICIGI",
+           "ICICIPRULI", "IDEA", "IDFCFIRSTB", "IGL", "INDIGO", "INDUSINDBK", "INDUSTOWER", "INFY", "IOC", "IRCTC",
+           "ITC", "JINDALSTEL", "JSWSTEEL", "JUBLFOOD", "KOTAKBANK", "L&TFH", "LALPATHLAB", "LICHSGFIN", "LT", "LTI",
+           "LTTS", "LUPIN", "M&M", "M&MFIN", "MANAPPURAM", "MARICO", "MARUTI", "MCDOWELL-N", "MFSL", "MGL", "MINDTREE",
+           "MOTHERSUMI", "MPHASIS", "MRF", "MUTHOOTFIN", "NAM-INDIA", "NATIONALUM", "NAUKRI", "NAVINFLUOR", "NESTLEIND",
+           "NMDC", "NTPC", "ONGC", "PAGEIND", "PEL", "PETRONET", "PFC", "PFIZER", "PIDILITIND", "PIIND", "PNB",
+           "POWERGRID", "PVR", "RAMCOCEM", "RBLBANK", "RECLTD", "RELIANCE", "SAIL", "SBILIFE", "SBIN", "SHREECEM",
+           "SIEMENS", "SRF", "SRTRANSFIN", "SUNPHARMA", "SUNTV", "TATACHEM", "TATACONSUM", "TATAMOTORS", "TATAPOWER",
+           "TATASTEEL", "TCS", "TECHM", "TITAN", "TORNTPHARM", "TORNTPOWER", "TRENT", "TVSMOTOR", "UBL", "ULTRACEMCO",
+           "UPL", "VEDL", "VOLTAS", "WIPRO", "ZEEL"]
+
+st.title("Open Interest")
+try:
+    selected_symbol = st.sidebar.selectbox("Select Tiker", symbols)
+    logger.info(f'Will fetch the data for {selected_symbol}')
+    data = n.equity_option_chain(selected_symbol)
+    data = pd.json_normalize(data['filtered']['data'])
+    # logger.info(data)
+    data_for_chart = data
+    data_for_chart.index = data_for_chart.get("strikePrice")
+    data_for_chart.drop(
+        data_for_chart.columns.difference(
+            ["CE.openInterest", "PE.openInterest"]
+        ),
+        1,
+        inplace=True,
+    )
+    st.write("Open Interest")
+    st.bar_chart(data_for_chart)
+    data_for_changeinOpenInterest = n.equity_option_chain(selected_symbol)
+    data_for_changeinOpenInterest = pd.json_normalize(data_for_changeinOpenInterest['filtered']['data'])
+    # logger.info(data_for_changeinOpenInterest)
+    data_for_changeinOpenInterest.index = data_for_changeinOpenInterest.get("strikePrice")
+    data_for_changeinOpenInterest.drop(
+        data_for_changeinOpenInterest.columns.difference(
+            ["CE.changeinOpenInterest", "PE.changeinOpenInterest"]
+        ),
+        1,
+        inplace=True,
+    )
+    st.write("Changed Open Interest")
+    st.bar_chart(data_for_changeinOpenInterest)
+except Exception as e:
+    st.error(e)
+    print(e)
